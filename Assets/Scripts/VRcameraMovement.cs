e7b7a60ca46e1f5f5b21fb39d2888230bf3b7eb5
RiikkaKilp <RiikkaKilp@users.noreply.github.com>
2018-04-23 11:33:34 +0300

    Initial commit

---
+++ Assets/Scripts/VRcameraMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRcameraMovement : MonoBehaviour {
+
+    public Transform vrCamera;
+    public float moveSpeed = 5.0f;
+
+    private CharacterController CharCont;
+
+    // Use this for initialization
+    void Start () {
+
+        CharCont = GetComponent<CharacterController>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 forward = vrCamera.TransformDirection(Vector3.forward);
+
+            CharCont.SimpleMove(forward * moveSpeed * Time.deltaTime);
+
+        }
+
+
+    }
+}
